a34db3206fce7a0eeea76f361b276cdefb950095
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-03-31 10:29:44 -0300

    Add books by id route

---
+++ models/book.js
@@ -0,0 +1,17 @@
+// models/book.js
+
+const mongoose = require("mongoose");
+const Schema   = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: String,
+  description: String,
+  author: String,
+  rating: Number
+}, {
+  timestamps: true
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Book = require('../models/book');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+// books
+router.get('/books', (req, res, next) => {
+  Book
+    .find()
+    .then(books => {
+      // console.log(booksFromMongoDB);
+      res.render('books', {
+        books
+      });
+
+    })
+    .catch(error => console.log(error));
+});
+
+// book detail route
+
+router.get('/book/:bookId', (req, res) => {
+  const {
+    bookId
+  } = req.params;
+
+  Book
+    .find({
+      _id: bookId
+    })
+    .then(book => {
+      console.log(book);
+      res.render('book-details', {
+        book: book[0]
+      });
+    })
+    .catch(error => console.log(error));
+})
+
+
 
 module.exports = router;
